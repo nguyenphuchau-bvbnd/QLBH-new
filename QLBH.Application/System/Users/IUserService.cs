05fc3e3ba5af50324f36a7aa0c11096a9d483ec5
Nguyen Phuc Hau <nguyenphuchau.bvbnd@gmail.com>
2021-08-13 18:45:11 +0700

    Authentication

--- QLBH.Application/System/Users/IUserService.cs
+++ QLBH.Application/System/Users/IUserService.cs
@@ -9,19 +9,7 @@ namespace QLBH.Application.System.Users
 {
     public interface IUserService
     {
-        //Task<string> Authencate(LoginRequest request);
-        Task<ApiResult<string>> Authencate(LoginRequest request);
-
-        Task<ApiResult<bool>> Register(RegisterRequest request);
-
-        //Task<ApiResult<bool>> Update(Guid id, UserUpdateRequest request);
-
-        //Task<ApiResult<PagedResult<UserVm>>> GetUsersPaging(GetUserPagingRequest request);
-
-        //Task<ApiResult<UserVm>> GetById(Guid id);
-
-        //Task<ApiResult<bool>> Delete(Guid id);
-
-        //Task<ApiResult<bool>> RoleAssign(Guid id, RoleAssignRequest request);
+        Task<string> Authencate(LoginRequest request);
+        Task<bool> Register(RegisterRequest request);
     }
 }
